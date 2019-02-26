a1b0d75a9600a4e2d6dd3e028b10568aa1ec51cd
Qiang Zhu <alecfans@gmail.com>
2019-02-26 10:34:45 -0800

    add script to visualize the data

---
+++ plot.py
@@ -0,0 +1,41 @@
+import numpy as np
+from math import sqrt, pi
+import sys
+import matplotlib.pyplot as plt
+from scipy.interpolate import interp1d
+from monty.serialization import loadfn, MontyDecoder,MontyEncoder
+import json
+
+plt.style.use("bmh")
+
+def plot_results(json_data, lists=[0], figname='result.png', title=None):
+# plot the results
+    fig = plt.gcf()
+    fig.set_size_inches(10, 8)
+    ax1 = plt.subplot(211)
+    for i in lists:
+        data = json_data[i]
+        eig_str = "{:d} {:6.2f}".format(data['eigenvalue'][0], data['eigenvalue'][1])
+        ax1.plot(data['potential'][:, 0], data['wavefunction'], '--', label=eig_str)
+    ax1.set_ylabel('$\Psi(x)$')
+    ax1.legend(loc=2)
+    plt.setp(ax1.get_xticklabels(), visible=False)
+
+
+    ax2 = plt.subplot(212, sharex=ax1)
+    for i in lists:
+        data = json_data[i]
+        ax2.hlines(y=data['eigenvalue'][1], xmin=data['potential'][0,0], xmax=data['potential'][-1,0], linewidth=1)
+        ax2.plot(data['potential'][:, 0], data['potential'][:, 1], 'b-')
+    ax2.set_ylabel('$V(x)$')
+    ax2.set_xlabel('$x$')
+    #ax2.set_ylim([0, 2*max(eigs)])
+    plt.tight_layout()
+    if title:
+        plt.title(title)
+    plt.savefig(figname)
+    plt.close()
+
+json_data = loadfn('trainingdata.json', cls=MontyDecoder)
+for i in range(len(json_data)):
+    plot_results(json_data, [i], figname='figs/'+str(i)+'.png', title=str(i))
